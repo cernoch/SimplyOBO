e70c132176c1f19963a49ec10890f8b352c3ff28
Radomír Černoch <radomir.cernoch@gmail.com>
2011-07-01 11:42:18 +0200

    Ontology supports logging and is documented

--- src/cz/cvut/felk/ida/simplyobo/ontology/Ontology.java
+++ src/cz/cvut/felk/ida/simplyobo/ontology/Ontology.java
@@ -35,65 +35,127 @@ import cz.cvut.felk.ida.simplyobo.tools.WithID;
 import java.io.IOException;
 import java.io.Reader;
 import java.text.NumberFormat;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
+import static java.util.logging.Level.*;
 
 /**
- * Ontology reads to a stanza parser and creates types and terms.
- *
+ * Ontology holds Types and Terms; allows loading from a StanzaParser.
+ * 
+ * <p>The ontology contains only axioms directly stated in the serialized file.
+ * If you want to use property axioms (such as transitivity), use
+ * {@link Reasoner}.</p>
+ * 
  * @author Radomír Černoch (radomir.cernoch at gmail.com)
  */
 public class Ontology implements StanzaListener {
+    
+    private static final Logger L = Logger.getLogger(Ontology.class.getName());
 
+    /**
+     * Creates an empty ontology.
+     */
     public Ontology() {}
 
+    /**
+     * Creates and ontology and loads objects from the given file.
+     * 
+     * @param r ontology in OBO format to be loaded
+     * @throws IOException if the Reader cannot be read
+     */
     public Ontology(Reader r) throws IOException {
         new LineByLineParser(new StanzaCollector(this)).parse(r);
     }
     
+    protected Namespace defNS = null;
+    
     @Override
-    public void onHeader(List<TagValuePair> header) {}
+    public void onHeader(List<TagValuePair> header) {
+        for (TagValuePair tvp : header) {
+            if ("format-version".equals(tvp.tag())) {
+                if (!"1.2".equals(tvp.val()))
+                    L.log(WARNING, "OBO ontology version " + tvp.val()
+                            + " not supported. Trying to recover.");
+                continue;
+            }
+
+            if ("default-namespace".equals(tvp.tag())) {
+                L.log(WARNING, "Setting default namespace: " + tvp.val());
+                try {
+                    defNS = Namespace.valueOf(tvp.val());
+                    
+                } catch(IllegalArgumentException ex) {
+                    throw new SyntaxError(
+                          "Not a valid namespace: \"" + tvp.val() + "\".", ex);
+                }
+                continue;
+            }
+        }
+    }
 
     @Override
     public void onStanza(String name, List<TagValuePair> tagVals) {
         if ("Term".equals(name)) {
+            L.log(FINE, "Creating a term from " + tagVals);
             createTerm(tagVals);
             return;
         }
         
         if ("Typedef".equals(name)) {
+            L.log(FINE, "Creating a type from " + tagVals);
             createTypedef(tagVals);
             return;
         }
     }
 
+    /**
+     * Finds the Type with the given ID in the ontology.
+     * 
+     * @param typeId ID of the searched Type
+     * @return the Type with given ID or {@code null} if no such Type exists
+     */
     public Type findType(String typeId) {
-        return types.get(typeId);
+        return types.contains(typeId) ? types.get(typeId) : null;
     }
     
+    /**
+     * Finds the Term with the given ID in the ontology.
+     * 
+     * @param termId ID of the searched Term
+     * @return the Term with given ID or {@code null} if no such Term exists
+     */
     public Term findTerm(Integer termId) {
-        return terms.get(termId);
+        return terms.contains(termId) ? terms.get(termId) : null;
     }
     
-    public Collection<? extends Type> allTypes() {
+    /**
+     * Returns all Types in the ontology.
+     */
+    public Set<? extends Type> allTypes() {
         return types.all();
     }
     
-    public Collection<? extends Term> allTerms() {
+    /**
+     * Returns all Terms in the ontology.
+     */
+    public Set<? extends Term> allTerms() {
         return terms.all();
     }
     
+    /**
+     * Creates a new Type from the list of t-v pairs stored into the ontology.
+     */
     private void createTerm(List<TagValuePair> tagVals) {
         
         TermImpl term;
         {
             Integer id = null;
             String name = null;
-            Namespace namespace = null;
+            Namespace namespace = defNS;
 
             // Read non-structured information
             for (TagValuePair tvp : tagVals) {
@@ -109,7 +171,13 @@ public class Ontology implements StanzaListener {
                 }
 
                 if ("namespace".equals(tvp.tag())) {
-                    namespace = Namespace.valueOf(tvp.val());
+                    try {
+                        namespace = Namespace.valueOf(tvp.val());
+                        
+                    } catch(IllegalArgumentException ex) {
+                        throw new SyntaxError(
+                          "Not a valid namespace: \"" + tvp.val() + "\".", ex);
+                    }
                     continue;
                 }
             }
@@ -125,6 +193,7 @@ public class Ontology implements StanzaListener {
             
             if ("is_a".equals(tvp.tag())) {
                 Integer id = goIDtoInt(tvp.val().trim());
+                L.log(FINER, "Parsing 'is_a' tag: " + id);
                 TermImpl other = terms.get(id);
                 subTerms.add(term, other);
                 supTerms.add(other, term);
@@ -132,6 +201,8 @@ public class Ontology implements StanzaListener {
             }
             
             if ("relationship".equals(tvp.tag())) {
+                L.log(FINER, "Parsing 'relationship' tag: " + tvp.val());
+                
                 int space = tvp.val().indexOf(" ");
                 if (space == -1) throw new SyntaxError("Relationship tag  must"
                         + " contain a typedef name, a space and a term id");
@@ -151,6 +222,9 @@ public class Ontology implements StanzaListener {
     protected SVOidx<TermImpl,TypeImpl,TermImpl> svoIdx
         = new SVOidx<TermImpl,TypeImpl,TermImpl>();
     
+    /**
+     * Creates a new Type from the list of t-v pairs stored into the ontology.
+     */
     private void createTypedef(List<TagValuePair> tagVals) {
         
         TypeImpl type;
@@ -188,21 +262,34 @@ public class Ontology implements StanzaListener {
         for (TagValuePair tvp : tagVals) {
             
             if ("is_a".equals(tvp.tag())) {
-                TypeImpl other = types.get(tvp.val().trim());
+                String id = tvp.val().trim();
+                L.log(FINER, "Parsing 'is_a' tag: " + id);
+                TypeImpl other = types.get(id);
                 subTypes.add(type, other);
                 supTypes.add(other, type);
                 continue;
             }
             
             if ("transitive_over".equals(tvp.tag())) {
-                TypeImpl transOverType = types.get(tvp.val().trim());
+                String id = tvp.val().trim();
+                L.log(FINER, "Parsing 'transitive_over' tag: " + id);
+                TypeImpl transOverType = types.get(id);
                 type.trOver.add(transOverType);
                 continue;
             }
         }
     }
     
-    private Integer goIDtoInt(String ident) {
+    /**
+     * Converts a string GO identified to a integer counterpart.
+     * 
+     * @param ident string representation of the id "GO:0000013"
+     * @return number representation of the id
+     * @throws NumberFormatException if the specified ID is not a numbers
+     */
+    private Integer goIDtoInt(String ident) throws NumberFormatException {
+        L.log(FINEST, "Converting GO identifier to number: " + ident);
+        
         if (ident.startsWith("GO:"))
             return Integer.valueOf(ident.substring(3));
         
@@ -326,6 +413,7 @@ public class Ontology implements StanzaListener {
     private class TermBuilder implements Builder<Integer,TermImpl> {
         @Override
         public TermImpl build(Integer id) {
+            L.log(FINER, "Creating a new Term with ID: " + id);
             return new TermImpl(id);
         }
     }
@@ -333,6 +421,7 @@ public class Ontology implements StanzaListener {
     private class TypeBuilder implements Builder<String,TypeImpl> {
         @Override
         public TypeImpl build(String id) {
+            L.log(FINER, "Creating a new Type with ID: " + id);
             return new TypeImpl(id);
         }
     }
